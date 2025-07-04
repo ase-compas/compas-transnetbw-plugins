ff9e831953f9dce5729a487fb036b07a3e4632f2
philippilievskibearingpointcom <philipp.ilievski@bearingpoint.com>
2025-07-04 13:46:57 +0200

    feature: add engineering-wizard plugin initial test

---
+++ apps/engineering-wizard/vite.config.ts
@@ -0,0 +1,45 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/engineering-wizard',
+
+  server: {
+    port: 4201,
+    host: 'localhost',
+    fs: {
+      allow: ['..'],
+    },
+  },
+
+  preview: {
+    port: 4301,
+    host: 'localhost'
+  },
+
+  plugins: [svelte(), nxViteTsPaths()],
+
+  build: {
+    outDir: '../../dist/apps/engineering-wizard',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+      },
+    },
+    lib: {
+      entry: 'src/plugin.ts',
+      formats: ['es'],
+      fileName: 'index',
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@smui/select'],
+  },
+});
