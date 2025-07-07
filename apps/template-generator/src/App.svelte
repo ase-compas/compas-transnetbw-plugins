22ec55038d7567a6835dee2226b72939468442e9
Lorenz Hörburger <lorenz.hoerburger@bearingpoint.com>
2025-07-07 16:14:37 +0200

    feature: add template-generator plugin initial test

---
+++ apps/template-generator/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div class="template-generator-container">
+  Hello World!
+</div>
+
+<style>
+  @import "/global.css";
+  @import "/material-icon.css";
+  @import '/smui.css';
+</style>
