b9c7f10d63d41af2e0258dbf5ae97a743305d268
philippilievskibearingpointcom <philipp.ilievski@bearingpoint.com>
2025-08-04 13:55:15 +0200

    feature: add initial engineering-process view

---
+++ apps/engineering-process/src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let doc: XMLDocument | undefined;
+  export let editCount = -1;
+  export let host: HTMLElement;
+
+</script>
+<p>test</p>
+<style>
+</style>
---
+++ apps/engineering-process/src/plugin.svelte
@@ -0,0 +1,16 @@
+<svelte:options tag={null} />
+<script lang="ts">
+  import * as pckg from '../package.json';
+  import App from './App.svelte';
+
+  export let doc: XMLDocument;
+  export let dev = false;
+  export let editCount = 0;
+</script>
+
+{#if doc || dev}
+    <App { doc } { editCount }></App>
+{/if}
+
+<input type="hidden" name="package-name" value={pckg.name} />
+<input type="hidden" name="package-version" value={pckg.version} />
