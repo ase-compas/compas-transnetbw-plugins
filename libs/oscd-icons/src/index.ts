373a70fabfe38e1f6de0cdbbae38576c64d4f321
Lorenz Hörburger <lorenz.hoerburger@bearingpoint.com>
2025-08-22 14:45:41 +0200

    feature: add OscdWandStarsIcon component

--- libs/oscd-icons/src/index.ts
+++ libs/oscd-icons/src/index.ts
@@ -16,3 +16,4 @@ export { default as OscdErrorIcon } from './oscd-error-icon/OscdErrorIcon.svelte
 export { default as OscdWarningIcon } from './oscd-warning-icon/OscdWarningIcon.svelte';
 export { default as OscdPlayCircleIcon } from './oscd-play-circle-icon/OscdPlayCircleIcon.svelte';
 export { default as OscdInfoIcon } from './oscd-info-icon/OscdInfoIcon.svelte';
+export { default as OscdWandStarsIcon } from './oscd-wand-stars-icon/OscdWandStarsIcon.svelte';
