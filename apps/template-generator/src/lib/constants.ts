6beb07803970f48d80125b7687501c834782562d
Lorenz Hörburger <lorenz.hoerburger@bearingpoint.com>
2025-09-04 16:41:48 +0200

    add: constants for namespace and private instanceType

---
+++ apps/template-generator/src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const NAMESPACE = 'transnetBW';
+
+// Namespace for <Private> elements used to add instanceType information
+// to DAType and EnumType elements
+export const PRIVATE_INSTANCE_TYPE_NS = `${NAMESPACE}:instanceType`;
