bfc98fa0b57f13c5d8f0429e40dc0e3f689e2544
philippilievskibearingpointcom <philipp.ilievski@bearingpoint.com>
2025-10-13 13:18:43 +0200

    refactor: add OscdCompareArrowsIcon and integrate it into process definition view

--- apps/engineering-wizard/src/views/engineering-process-detail/process-definition.view.svelte
+++ apps/engineering-wizard/src/views/engineering-process-detail/process-definition.view.svelte
@@ -3,6 +3,7 @@
   import { PluginGroup, Process } from 'libs/shared/src';
   import { onMount } from 'svelte';
   import ProcessExternalPluginList from '../../components/engineering-process-detail/ProcessExternalPluginList.svelte';
+  import { OscdCompareArrowsIcon } from '../../../../../libs/oscd-icons/src';
 
   export let pluginGroups: PluginGroup[] = [];
   export let proc: Process | null = null;
@@ -16,16 +17,16 @@
   });
 
   onMount(() => {
-    // const pluginsStr = localStorage.getItem('plugins');
-    // try {
-    //   const plugins = pluginsStr ? JSON.parse(pluginsStr) : [];
-    //   pluginNames = (Array.isArray(plugins) ? plugins : [])
-    //     .filter(p => p?.kind === 'editor')
-    //     .map(p => p?.name);
-    // } catch (err) {
-    //   console.error('Invalid JSON in localStorage key "plugins"', err);
-    //   pluginNames = [];
-    // }
+    const pluginsStr = localStorage.getItem('plugins');
+    try {
+      const plugins = pluginsStr ? JSON.parse(pluginsStr) : [];
+      pluginNames = (Array.isArray(plugins) ? plugins : [])
+        .filter(p => p?.kind === 'editor')
+        .map(p => p?.name);
+    } catch (err) {
+      console.error('Invalid JSON in localStorage key "plugins"', err);
+      pluginNames = [];
+    }
   });
 </script>
 
@@ -37,16 +38,14 @@
   />
 
   {#if pluginNames.length}
+    <div class="drag-and-drop-info">
+      <OscdCompareArrowsIcon svgStyles="fill: #6B9197"></OscdCompareArrowsIcon>
+      <p>SELECT OR DRAG & DROP PLUGINS</p>
+    </div>
+
     <ProcessExternalPluginList pluginNames={filteredPluginNames} bind:searchTerm />
   {/if}
-
-<!--  <div class="plugin-list">-->
-<!--    <OscdCardItem variant="secondary" text="test"></OscdCardItem>-->
-<!--    <OscdCardItem variant="secondary" text="test1"></OscdCardItem>-->
-<!--    <OscdCardItem variant="secondary" text="test2"></OscdCardItem>-->
-<!--  </div>-->
 </div>
-
 
 <style>
   .process-definition-view {
@@ -54,5 +53,19 @@
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+  }
+
+  .drag-and-drop-info{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 500px;
+    gap: 12px;
+  }
+
+  .drag-and-drop-info p{
+    color: #6B9197;
+    font-weight: 500;
   }
 </style>
