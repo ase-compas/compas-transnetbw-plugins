88115e0c103b5a690f6c8acf4cf2ba58fef75b5b
Lorenz Hörburger <lorenz.hoerburger@bearingpoint.com>
2026-01-19 10:39:55 +0100

    feature(id-generator): change pseudo-UUID with real UUID generator

--- libs/oscd-services/src/id-generator/id-generator.ts
+++ libs/oscd-services/src/id-generator/id-generator.ts
@@ -111,9 +111,5 @@ export class OscdIdGenerator implements IdGenerator {
  * @returns A randomly generated UUID string.
  */
 export function generateUUID(): string {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    const r = Math.random() * 16 | 0;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
+  return crypto.randomUUID();
 }
