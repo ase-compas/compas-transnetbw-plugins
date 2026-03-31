f42aec29396164359a629a88c9880ba1fb928bba
Lorenz Hörburger <lorenz.hoerburger@bearingpoint.com>
2026-03-31 11:56:44 +0200

    chore: export custom-resource-api-client

--- libs/api/compas/custom-resource/src/index.ts
+++ libs/api/compas/custom-resource/src/index.ts
@@ -1,1 +1,10 @@
+import { createCustomResourceClient } from './lib/client';
+import { createCustomResourceService } from './lib/service';
+
 export * from './lib/client';
+export * from './lib/service'
+export * from './generated/models'
+export {
+    UploadDataContentTypeEnum,
+    UploadDataNextVersionTypeEnum
+} from './generated';
